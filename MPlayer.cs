8dc1213b68274ffe0673a29af76503689b29668f
Nothrim <ibartek94@gmail.com>
2015-05-05 00:30:20 +0200

    tAPI(Terraria) mod
    tAPI(Terraria) mod

---
+++ MPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TAPI;
+using Terraria;
+namespace ProjectAdvance
+{
+    class MPlayer : ModPlayer
+    {
+        public override void OnHit(NPC victim, Microsoft.Xna.Framework.Vector2 location)
+        {
+            Main.NewText(victim.ToString());
+            base.OnHit(victim, location);
+        }
+    }
+}
